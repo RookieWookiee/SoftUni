cb9358d3de9f76a47fdc39eef012d1f72d4ab2ec
RookieWookiee <yavorlulchev@gmail.com>
2017-02-04 22:55:35 +0200

    Removed a package. Renamed few variables

--- Java Advanced January 2017/01.StacksAndQueues/src/_13_EvaluateExpression.java
+++ Java Advanced January 2017/01.StacksAndQueues/src/_13_EvaluateExpression.java
@@ -1,5 +1,4 @@
 
-import java.util.ArrayDeque;
 import java.util.Scanner;
 import java.util.Stack;
 import java.util.regex.Pattern;
@@ -9,26 +8,27 @@ public class _13_EvaluateExpression {
 	private static final String NUM_OR_VAR = "[a-z0-9]+";
 	private static final String NUM = "[0-9]+";
 	private static final String OPERATORS = "[*+-/]";
-
+	private static final String SPLIT_DELIM = "\\s+";
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String result = _12_InfixToPostfix.infixToPrefix(sc.nextLine());
-		System.out.format("%.2f\n", EvaluatePrefix(result));
+		String prefix = _12_InfixToPostfix.infixToPrefix(sc.nextLine());
+		System.out.format("%.2f\n", EvaluatePrefix(prefix));
 	}
 
 	public static Double EvaluatePrefix(String prefix) {
-		String[] s = prefix.split(" ");
+		String[] tokens = prefix.split(SPLIT_DELIM);
 		Stack<Double> output = new Stack();
 		Stack<String> opStack = new Stack();
 
-		for (int i = 0; i < s.length; ++i) {
-			if (Pattern.matches(NUM, s[i])) {
-				output.push(Double.parseDouble(s[i]));
+		for (int i = 0; i < tokens.length; ++i) {
+			if (Pattern.matches(NUM, tokens[i])) {
+				output.push(Double.parseDouble(tokens[i]));
 			}
-			if (Pattern.matches(OPERATORS, s[i])) {
+			if (Pattern.matches(OPERATORS, tokens[i])) {
 				Double y = output.pop();
 				Double x = output.pop();
-				switch (s[i]) {
+				switch (tokens[i]) {
 					case "+":
 						output.push(x + y);
 						break;
