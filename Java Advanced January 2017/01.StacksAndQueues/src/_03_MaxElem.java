74ccc0517f36d58ae167eb17f180fd95e936eec7
RookieWookiee <yavorlulchev@gmail.com>
2017-02-04 02:16:48 +0200

    Updated tracking of biggest value. Not relying on a additional sorted stack for finding it.

--- Java Advanced January 2017/01.StacksAndQueues/src/_03_MaxElem.java
+++ Java Advanced January 2017/01.StacksAndQueues/src/_03_MaxElem.java
@@ -6,28 +6,37 @@ import java.util.Stack;
 public class _03_MaxElem {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Stack stack = new Stack();
-        ArrayList output = new ArrayList<Integer>();
+        Stack<Integer> stack = new Stack();
+        Stack<Integer> maxElements = new Stack();
+        int maxVal = Integer.MIN_VALUE;
+        
         
         int lines = Integer.parseInt(in.nextLine());
         for(int i = 0; i < lines; ++i) {
-            String input = in.nextLine();
-            switch(input.charAt(0)) {
-                case '1':
-                    stack.add(Integer.parseInt(input.split(" ")[1]));
+            String[] input = in.nextLine().split(" ");
+            switch(input[0]) {
+                case "1":
+                    int num = Integer.parseInt(input[1]);
+                    stack.push(num);
+                    if(num >= maxVal)
+                    {
+                        maxVal = num;
+                        maxElements.push(num);
+                    }
                     break;
-                case '2':
-                    stack.pop();
+                case "2":
+                    if(maxVal == stack.pop()) {
+                        maxElements.pop();
+                        if(!maxElements.isEmpty())
+                            maxVal = maxElements.peek();
+                        else
+                            maxVal = Integer.MIN_VALUE;
+                    }
                     break;
-                case '3':
-                    Stack temp = (Stack)stack.clone();
-                    temp.sort(null);
-                    output.add(temp.pop());
+                case "3":
+                    System.out.println(maxElements.peek());
                     break;
             }
         }
-        output.forEach((ob) -> {
-            System.out.println(ob);
-        });
     }
 }
